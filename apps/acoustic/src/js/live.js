5337dbe7eaa3728d1aa42d06759a830ac308417d
ponyspy <desade4me@gmail.com>
2019-03-27 19:38:12 +0300

    maket

--- apps/acoustic/src/js/live.js
+++ apps/acoustic/src/js/live.js
@@ -2,7 +2,7 @@ $(window).on('load hashchange', function(e) {
 	if (!location.hash || location.hash == '#') return false;
 
 	$.post('', { id: location.hash.replace('#', '') }).done(function(data) {
-		$('.panel_block.right').empty().append(data);
+		$('.panel_block.right').children('.panel_inner').empty().append(data);
 	});
 });
 
