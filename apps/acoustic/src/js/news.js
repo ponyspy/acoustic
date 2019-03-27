9ed37ebd4d11d5e63f62ad9cb8125ebda1e5aee6
ponyspy <desade4me@gmail.com>
2019-03-27 19:41:01 +0300

    fix

--- apps/acoustic/src/js/news.js
+++ apps/acoustic/src/js/news.js
@@ -2,6 +2,6 @@ $(window).on('load hashchange', function(e) {
 	if (!location.hash || location.hash == '#') return false;
 
 	$.post('', { id: location.hash.replace('#', '') }).done(function(data) {
-		$('.panel_block.right').empty().append(data);
+		$('.panel_block.right').children('.panel_inner').empty().append(data);
 	});
 });
