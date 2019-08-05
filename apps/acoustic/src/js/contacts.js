7eacd370b2ed0717ef2de1d6cfd47ca9b2c473ff
ponyspy <desade4me@gmail.com>
2019-08-05 18:54:50 +0300

    fix

--- apps/acoustic/src/js/contacts.js
+++ apps/acoustic/src/js/contacts.js
@@ -1,10 +1,17 @@
+// $(function() {
+// 	$('a').each(function() {
+// 		var $this = $(this);
+// 		var href = $this.attr('href').split('::');
+
+// 		if (href[0] == 'nw') {
+// 			$this.attr({'target': '_blank', 'href': href[1]});
+// 		}
+// 	});
+// });
+
+
 $(function() {
 	$('a').each(function() {
-		var $this = $(this);
-		var href = $this.attr('href').split('::');
-
-		if (href[0] == 'nw') {
-			$this.attr({'target': '_blank', 'href': href[1]});
-		}
+		$(this).attr('target', '_blank');
 	});
 });
